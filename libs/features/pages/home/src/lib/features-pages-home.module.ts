355c9a202d1f49ba89e56f950c7eead46e97723d
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-08-29 22:47:11 +0300

    new changes

--- libs/features/pages/home/src/lib/features-pages-home.module.ts
+++ libs/features/pages/home/src/lib/features-pages-home.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './main/home/home-page.component';
 
 import { HomeRoutingModule } from './home.routing';
-
+import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
+import { ElementsFooterModule } from '@elewa-website/elements/footer';
 @NgModule({
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule,ElementsFooterModule,ButtonsModule],
   declarations: [HomePageComponent],
   exports: [HomePageComponent],
 })
