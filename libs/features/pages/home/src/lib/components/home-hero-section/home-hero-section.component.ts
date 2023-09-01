3e764a14b14d45a3b8b3ba0a4bdfc4aab71aa5de
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-09-01 15:46:35 +0300

    :art:import the home hero section

--- libs/features/pages/home/src/lib/components/home-hero-section/home-hero-section.component.ts
+++ libs/features/pages/home/src/lib/components/home-hero-section/home-hero-section.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+  
+import { TextsModule } from '@elewa-website/elements/layout/texts';
+import { AppHeaderModule } from '@elewa-website/elements/layout/header';
 @Component({
   selector: 'elewa-website-home-hero-section',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,TextsModule,AppHeaderModule],
   templateUrl: './home-hero-section.component.html',
   styleUrls: ['./home-hero-section.component.scss'],
 })
--- libs/features/pages/home/src/lib/features-pages-home.module.ts
+++ libs/features/pages/home/src/lib/features-pages-home.module.ts
@@ -6,16 +6,16 @@ import { AppHeaderModule } from '@elewa-website/elements/layout/header';
 
 import { HomePageComponent } from './main/home/home-page.component';
 import { NewsSectionComponent } from './components/news-section/news-section.component';
-
+import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
 import { HomeRoutingModule } from './home.routing';
-//import the footer module 
+
 import { ElementsLayoutFooterModule } from '@elewa-website/elements/layout/footer';
-//import slider button module
+
 import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
 
 
 @NgModule({
-  imports: [CommonModule, HomeRoutingModule, CardsModule, AppHeaderModule,ButtonsModule,ElementsLayoutFooterModule],
+  imports: [CommonModule, HomeRoutingModule, CardsModule, AppHeaderModule,ButtonsModule,ElementsLayoutFooterModule,HomeHeroSectionComponent],
   declarations: [HomePageComponent, NewsSectionComponent],
   exports: [HomePageComponent],
 })
