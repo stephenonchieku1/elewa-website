6ed9c08e8f8a1998a1c64faff09e7992aa6e9c67
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-09-01 15:05:49 +0300

    :sparkles: genrated standalone component

---
+++ libs/features/pages/home/src/lib/components/home-hero-section/home-hero-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'elewa-website-home-hero-section',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home-hero-section.component.html',
+  styleUrls: ['./home-hero-section.component.scss'],
+})
+export class HomeHeroSectionComponent {}
