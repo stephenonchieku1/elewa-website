2e5c35810af386860f5fc63c9f8417ea417a8ce9
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-09-07 10:49:17 +0300

    :fire:delete import and standalone

--- libs/features/pages/home/src/lib/components/home-hero-section/home-hero-section.component.ts
+++ libs/features/pages/home/src/lib/components/home-hero-section/home-hero-section.component.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'elewa-website-home-hero-section',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './home-hero-section.component.html',
   styleUrls: ['./home-hero-section.component.scss'],
 })
